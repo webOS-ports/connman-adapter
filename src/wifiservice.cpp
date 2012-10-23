030c6d8b5fb4d2e2e25134d5fd8ba53dab1cc405
Simon Busch <morphis@gravedo.de>
2012-10-23 07:44:31 +0200

    Add copyright link to libconnman-qt for some copied peaces
    Open-webOS-DCO-1.0-Signed-off-by: Simon Busch <morphis@gravedo.de>

--- src/wifiservice.cpp
+++ src/wifiservice.cpp
@@ -3,6 +3,11 @@
  *
  * Copyright (c) 2012 Simon Busch <morphis@gravedo.de>
  *
+ * Some of the code is fairly copied from libconnman-qt under the terms of the Apache 2.0
+ * license and is
+ * Copyright (c) 2010, Intel Corporation.
+ * Copyright (c) 2012, Jolla.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
